8c0b6c929a16ca15a6ea4c3752fdd2cf538f6a07
Piotr Sokołowski <piotrsokolowski00@outlook.com>
2020-11-02 09:57:25 +0100

    Empty sketch

---
+++ test/test.ino
@@ -0,0 +1,20 @@
+/**
+ * @file test.ino
+ * @author Piotr Sokołowski (psokolowski@student.agh.edu.pl)
+ * @brief Firmware to test all functions of God's eye device
+ * @version 0.1
+ * @date 2020-11-02
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+void setup()
+{
+    // code...
+}
+
+void loop()
+{
+    // code...
+}
