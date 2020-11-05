2b332caf92ac2f6652d17e823a5e1260a7d3fcf2
Piotr Sokołowski <piotrsokolowski00@outlook.com>
2020-11-05 22:32:57 +0100

    Added test of the Wi-fi connection

--- test/test.ino
+++ test/test.ino
@@ -9,14 +9,39 @@
  * 
  */
 
+#include <ESP8266WiFi.h>
 #include "./defines.hpp"
+
+String ssid = "MW40V_D69E",
+       passwd = "91530386";
 
 void setup()
 {
+    WiFi.mode(WIFI_OFF);     // For some reasons, Wi-fi should be turned off before any actions
     pinMode(LED, OUTPUT);    // Set LED as output
     digitalWrite(LED, HIGH); // Turn LED off
     Serial.begin(9600);      // Enable serial connection at baudrate 9600
-    Serial.println();        // After boot there are some chars, so let's go to new line before test
+    Serial.println();        // After boot there are some chars, so let's make empty line before test
+    Serial.println();
+    WiFi.mode(WIFI_STA);
+    Serial.println("Wi-fi enabled successfully");
+    WiFi.begin(ssid, password);
+    Serial.print("Connecting to ");
+    Serial.print(ssid);
+    Serial.println(" network");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print(".");
+        digitalWrite(2, LOW);
+        delay(250);
+        digitalWrite(2, HIGH);
+        delay(250);
+    }
+    Serial.println();
+    Serial.print("Connected successfully to ");
+    Serial.print(ssid);
+    Serial.println(" network");
+    Serial.println();
 }
 
 void loop()
