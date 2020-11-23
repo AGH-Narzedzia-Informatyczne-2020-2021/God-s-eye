da42d51f6e5aa8f7ebcb01ed8aaddca6ca331150
Piotr Sokołowski <piotrsokolowski00@outlook.com>
2020-11-23 19:57:03 +0100

    Removed code with errors, replaced LED functions

--- test/test.ino
+++ test/test.ino
@@ -11,38 +11,23 @@
 
 void setup()
 {
-    WiFi.mode(WIFI_OFF);     // For some reasons, Wi-fi should be turned off before any actions
-    pinMode(LED, OUTPUT);    // Set LED as output
-    digitalWrite(LED, HIGH); // Turn LED off
-    Serial.begin(115200);    // Enable serial connection at baudrate 115200
-    Serial.println();        // After boot there are some chars, so let's make empty line before test
+    WiFi.mode(WIFI_OFF);  // For some reasons, Wi-fi should be turned off before any actions
+    setupLED();           // Set LED as output
+    disableLED();         // Turn LED off
+    Serial.begin(115200); // Enable serial connection at baudrate 115200
+    Serial.println();     // After boot there are some chars, so let's make empty line before test
     Serial.println();
     WiFi.mode(WIFI_STA);
     Serial.println("Wi-fi enabled successfully");
-    network = scanWifi();
-    connected = connectTo(network)
-    Serial.println();
-    Serial.print("Connected successfully to ");
-    Serial.print(netwok);
-    Serial.println(" network");
-    Serial.println();
 }
 
 void loop()
 {
     // Let's display on monitor everything, what it gets!
-    if (Serial.available() > 0);
+    if (Serial.available() > 0)
     {
-        digitalWrite(LED, LOW);            // On data recieve, turn LED on
+        enableLED();                       // On data recieve, turn LED on
         Serial.print(Serial.readString()); // Print recieved string
-        digitalWrite(LED, HIGH);           // Turn LED off
+        disableLED();                      // Turn LED off
     }
-}
-
-void loop()
-{
-    if (connected)
-        Seria.print("Connected to wifi")
-    else
-        Serial.print("device is offline");
 }
